dbbca7eb991687132622aa7e28f87971a4f2b6fd
Mantiñán Montero <jmantinan@minsait.com>
2021-09-28 12:29:13 +0200

    Creacion de componentes

--- ej-angular/ejemplos-angular/src/app/app.component.ts
+++ ej-angular/ejemplos-angular/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplos-angular';
+  cuenta = 4;
 }
--- ej-angular/ejemplos-angular/src/app/app.module.ts
+++ ej-angular/ejemplos-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponenteAManoComponent } from './componente-a-mano/componente-a-mano.component';
+import { ComponenteAComponent } from './componente-a/componente-a.component';
+import { ComponenteBComponent } from './componente-b/componente-b.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteAManoComponent,
+    ComponenteAComponent,
+    ComponenteBComponent
   ],
   imports: [
     BrowserModule

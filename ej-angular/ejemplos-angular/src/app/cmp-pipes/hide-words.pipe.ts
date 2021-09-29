34b1981c95b20493d5dda86e5371d3a5a1653102
Mantiñán Montero <jmantinan@minsait.com>
2021-09-29 18:05:31 +0200

    Practica pipes profesor

--- ej-angular/ejemplos-angular/src/app/cmp-pipes/hide-words.pipe.ts
+++ ej-angular/ejemplos-angular/src/app/cmp-pipes/hide-words.pipe.ts
@@ -5,13 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HideWordsPipe implements PipeTransform {
 
-  transform(value: string, ...texto: string[]): string {
-    var nuevoTexto: string = value
-    for (var i = 0; i < texto.length; i++) {
-      let palabra = texto[i]
-      nuevoTexto = nuevoTexto.replace(new RegExp(palabra, 'gi'), '*'.repeat(texto[i].length))
-    }
-    return nuevoTexto;
+  // transform(value: string, ...texto: string[]): string {
+  //   var nuevoTexto: string = value
+  //   for (var i = 0; i < texto.length; i++) {
+  //     let palabra = texto[i]
+  //     nuevoTexto = nuevoTexto.replace(new RegExp(palabra, 'gi'), '*'.repeat(texto[i].length))
+  //   }
+  //   return nuevoTexto;
+  // }
+
+  transform(value: string, palabras: string[]): string {
+    palabras.forEach(palabra => {
+      const regExp = new RegExp(palabra,'gi');
+      value = value.replace(regExp, '*'.repeat(palabra.length));
+    });
+    return value;
   }
 
 }

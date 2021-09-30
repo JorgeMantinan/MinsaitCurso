a06fcf560647e57790d0e6995ea664aa325a1aa1
Mantiñán Montero <jmantinan@minsait.com>
2021-09-30 16:08:12 +0200

    directivas estructurales y trackBy

---
+++ ej-angular/ejemplos-angular/src/app/cmp-directivas/cmp-directivas.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmp-directivas',
+  templateUrl: './cmp-directivas.component.html',
+  styleUrls: ['./cmp-directivas.component.css']
+})
+export class CmpDirectivasComponent implements OnInit {
+
+  colorLetra = 'orange';
+  pintaElFondo = true;
+  darkMode = true;
+
+  // animales = ['perro','gato','canario','tortuga'];
+  animales2 = [{ id: 1, tipo: 'perro' },
+  { id: 2, tipo: 'gato' },
+  { id: 3, tipo: 'canario' },
+  { id: 4, tipo: 'tortuga' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toogleFondo() {
+    this.pintaElFondo = !this.pintaElFondo;
+  }
+
+  activaDarkMode() {
+    this.darkMode = true;
+  }
+  activaLightMode() {
+    this.darkMode = false;
+  }
+
+  getStyles() {
+    return {
+      textDecoration: 'underline',
+      color: this.colorLetra
+    }
+  }
+  getClases() {
+    return {
+      letraRoja: true,
+      fondoAmarillo: this.pintaElFondo
+    }
+  }
+
+  trackByAnimales(index: number, animal: string) {
+    console.log(index);
+    return index;
+  }
+
+  trackByAnimales2(index: number, animal: any) {
+    console.log(animal.id);
+    return animal.id;
+  }
+
+}
--- ej-angular/ejemplos-angular/src/app/cmp-pipes/cmp-pipes.component.ts
+++ ej-angular/ejemplos-angular/src/app/cmp-pipes/cmp-pipes.component.ts
@@ -15,10 +15,31 @@ export class CmpPipesComponent implements OnInit {
     apellido: 'Falco',
     email: 'cfalco@gmail.com'
   }
+//Resolve le damos como resuelta, reject como no resuelta
+  mensaje = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('El canario está en la jaula...')
+    }, 3000)
+  })
+
+  personaje = {};
+  personaje2: Promise<any> = new Promise(res => {});
 
   constructor() { }
 
   ngOnInit(): void {
+    fetch('https://swapi.dev/api/people/1')
+    .then( resp => {
+      return resp.json();
+    })
+    .then(datos => {
+      this.personaje = datos;
+    })
+
+    this.personaje2 = fetch('https://swapi.dev/api/people/2')
+    .then( resp => {
+      return resp.json();
+    })
   }
 
 }

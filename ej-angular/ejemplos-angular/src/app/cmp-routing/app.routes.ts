885b8aec4f61ca0e8e9068a0eefeda783a851cf5
Mantiñán Montero <jmantinan@minsait.com>
2021-10-05 14:07:07 +0200

    Lazy Loading Admin

--- ej-angular/ejemplos-angular/src/app/cmp-routing/app.routes.ts
+++ ej-angular/ejemplos-angular/src/app/cmp-routing/app.routes.ts
@@ -1,4 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin/admin.component';
 import { Error404Component } from './error404/error404.component';
 import { InfoUsuarioComponent } from './info-usuario/info-usuario.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
@@ -11,6 +12,7 @@ const RUTAS_APP: Routes = [
     { path: 'usuarios', component: ListaUsuariosComponent, children: RUTAS_USUARIOS},
     { path: 'nuevo-usuario', component: NuevoUsuarioComponent, canDeactivate: [NuevoUsuarioGuard] },
     // { path: 'usuarios/:usuarioId', component: InfoUsuarioComponent },
+    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(modulo => modulo.AdminModule) },
     { path: '', redirectTo: 'usuarios', pathMatch: 'full'},
     { path: '**', component: Error404Component }
 ]
